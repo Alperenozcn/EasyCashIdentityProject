d8730a63a2d41252f02ce41844ac81bffa38aab5
Alperenozcn <aperenozcn@iclod.com>
2023-05-18 17:11:11 +0300

    9. ders bitti

--- EasyCashIdentityProject.BusinessLayer/Concrete/CustomerAccountManager.cs
+++ EasyCashIdentityProject.BusinessLayer/Concrete/CustomerAccountManager.cs
@@ -20,27 +20,27 @@ namespace EasyCashIdentityProject.BusinessLayer.Concrete
 
         public void TDelete(CustomerAccount t)
         {
-            
+            _customerAccountDal.Delete(t);
         }
 
         public CustomerAccount TGetByID(int id)
         {
-            throw new NotImplementedException();
+            return _customerAccountDal.GetByID(id);
         }
 
         public List<CustomerAccount> TGetList()
         {
-            throw new NotImplementedException();
+            return _customerAccountDal.GetList();
         }
 
         public void TInsert(CustomerAccount t)
         {
-            throw new NotImplementedException();
+            _customerAccountDal.Insert(t);
         }
 
         public void TUpdate(CustomerAccount t)
         {
-            throw new NotImplementedException();
+            _customerAccountDal.Update(t);
         }
     }
 }
